de40860957aafaa583286a430d3483258ac3b45a
Alan Bautista <93614900+abautista-caylent@users.noreply.github.com>
2023-08-17 13:10:57 -0300

    Update install.sh

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -7,5 +7,5 @@ sudo mkdir -p /AtomicRedTeam/atomics/
 sudo cp -r TR-Blogs/atomic-red-docker/atomics-overrides /AtomicRedTeam/atomics
 sudo cp TR-Blogs/atomic-red-docker/RunTests.ps1 /root
 #sduo rm -r TR-Blogs
-echo 'curl -o /tmp/install_remote.sh https://raw.githubusercontent.com/ivanba-arch/redteam/main/install_d.sh && chmod +x /tmp/install_remote.sh && /tmp/install_remote.sh' >> /home/ubuntu/.bashrc
+echo 'curl -o /tmp/install_remote.sh https://raw.githubusercontent.com/abautista-caylent/launcher/main/ubuntu/install_d.sh && chmod +x /tmp/install_remote.sh && /tmp/install_remote.sh' >> /home/ubuntu/.bashrc
 echo "Script completed" > /tmp/script_status.log
