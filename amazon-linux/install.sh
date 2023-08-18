18f28fe822014937da0c56d19d5fd3c226570a14
Alan Bautista <93614900+abautista-caylent@users.noreply.github.com>
2023-08-18 00:43:17 -0300

    Update install.sh

--- amazon-linux/install.sh
+++ amazon-linux/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo yum update -y
-sudo yum install curl libunwind libicu libcurl openssl libuuid.x86_64 wget tar gzip --skip-broken
+sudo yum install -y curl libunwind libicu libcurl openssl libuuid.x86_64 git wget tar gzip --skip-broken
 wget https://raw.githubusercontent.com/PowerShell/PowerShell/master/docker/InstallTarballPackage.sh
 chmod +x InstallTarballPackage.sh
 sudo ./InstallTarballPackage.sh 7.3.6 powershell-7.3.6-linux-x64.tar.gz
-sudo yum install -y git
+#sudo yum install -y git
 git clone https://github.com/sysdig/TR-Blogs.git
 #pwsh -Command "Install-Module -Name powershell-yaml -Scope CurrentUser -Force"
 pwsh -Command "IEX (IWR 'https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicredteam.ps1' -UseBasicParsing); Install-AtomicRedTeam -getAtomics" -Force
