f03793c569bb731dbab478d3423a8bce5d940a92
Alan Bautista <93614900+abautista-caylent@users.noreply.github.com>
2023-08-18 00:25:08 -0300

    Update install.sh

--- amazon-linux/install.sh
+++ amazon-linux/install.sh
@@ -3,10 +3,10 @@ sudo yum update -y
 sudo yum install curl libunwind libicu libcurl openssl libuuid.x86_64 wget tar gzip --skip-broken
 wget https://raw.githubusercontent.com/PowerShell/PowerShell/master/docker/InstallTarballPackage.sh
 chmod +x InstallTarballPackage.sh
-sudo ./InstallTarballPackage.sh 7.2.7 powershell-7.2.7-linux-x64.tar.gz
+sudo ./InstallTarballPackage.sh 7.3.6 powershell-7.3.6-linux-x64.tar.gz
 sudo yum install -y git
 git clone https://github.com/sysdig/TR-Blogs.git
-pwsh -Command "Install-Module -Name powershell-yaml -Scope CurrentUser -Force"
+#pwsh -Command "Install-Module -Name powershell-yaml -Scope CurrentUser -Force"
 pwsh -Command "IEX (IWR 'https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicredteam.ps1' -UseBasicParsing); Install-AtomicRedTeam -getAtomics" -Force > /tmp/pwsh_output.log 2>&1
 sudo mkdir -p /AtomicRedTeam/atomics/
 sudo cp -r TR-Blogs/atomic-red-docker/atomics-overrides /AtomicRedTeam/atomics
